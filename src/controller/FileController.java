06f1086bfab0e54502792f00c038ef6c21d8a16f
ciroH <63865991+ciroH@users.noreply.github.com>
2021-06-17 17:57:11 -0300

    FileController can open a JFileChooser component

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -1,10 +1,18 @@
 package controller;
 
-public class FileController {
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+import entities.AudioFile;
+
+public class FileController {
+	AudioFile loadedFile;
+	
 	public void openFile() {
-		
-		
+		JFileChooser fileChooser = new JFileChooser();
+		FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("flac or mp3 file", "flac", "mp3");
+		fileChooser.setFileFilter(extensionFilter);
+		fileChooser.showOpenDialog(fileChooser);
 	}
 	
 }
