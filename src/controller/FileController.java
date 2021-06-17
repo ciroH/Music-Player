a28ceef0b36fe6f054ce088ef672e135b2f8f54e
ciroH <63865991+ciroH@users.noreply.github.com>
2021-06-17 18:18:59 -0300

    method openFile can now get a filePath

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -6,13 +6,18 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import entities.AudioFile;
 
 public class FileController {
-	AudioFile loadedFile;
+	AudioFile loadedFile = new AudioFile();
+	String filePath;
 	
 	public void openFile() {
 		JFileChooser fileChooser = new JFileChooser();
 		FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("flac or mp3 file", "flac", "mp3");
 		fileChooser.setFileFilter(extensionFilter);
-		fileChooser.showOpenDialog(fileChooser);
+		int valueReturned = fileChooser.showOpenDialog(fileChooser);
+		if (valueReturned == JFileChooser.APPROVE_OPTION) {
+			filePath = fileChooser.getSelectedFile().getPath();
+			loadedFile.setPath(filePath);
+		}
 	}
 	
 }
