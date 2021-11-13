2dea1c55fb3e469d694a5746abb817782ce27145
ciroH <63865991+ciroH@users.noreply.github.com>
2021-11-13 18:24:21 -0300

    Defined PlaylistController basic methods

--- src/controller/PlaylistController.java
+++ src/controller/PlaylistController.java
@@ -1,17 +1,49 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import audio.AudioProcessing;
 import ui.Player;
 
 public class PlaylistController {
 	Player uiRefInstance;
 	AudioProcessing audioManager;
-	
+	HashMap<Integer, String> playlist = new HashMap<>();
+	int index = -1;
 	
 	public PlaylistController(Player player, AudioProcessing audio) {
 		uiRefInstance = player;
-		audioManager = audio;
+		audioManager = audio;	
+	}
+
+	
+	
+	public HashMap<Integer, String> getPlaylist() {
+		return playlist;
+	}
+
+	
+	
+	public void setPlaylist(HashMap<Integer, String> recievedHashMap) {
+		if(this.playlist.isEmpty()) this.playlist = recievedHashMap;
+		else {
+			for (Map.Entry<Integer, String> element : recievedHashMap.entrySet()) {
+				this.playlist.put(element.getKey(), element.getValue());
+			}
+		}
 	}
 	
+	
+	public void clearPlaylist(){
+		playlist.clear();
+	}
+	
+	//TODO: move AudioProcessing loadPlaylist(), playlistStart(), playListIndex, and playlist to PlaylistController
+	
+	public void startPlaylist() {
+		
+		
+	}
 	
 }
